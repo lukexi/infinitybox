8fc511a1c83f1a2c7017f9291590e85dc4dd35c0
Luke Iannini <lukexi@me.com>
2015-06-17 13:06:41 -0700

    Todos/spacing

--- src/Client.hs
+++ src/Client.hs
@@ -112,16 +112,17 @@ main = do
     -- Handle network events
     readChanAll receiveChan interpret
 
-    -- Get Hydra update
+    -- Get latest Hydra data
     hands <- getHands
 
     -- Handle mouse events
     isFocused <- getWindowFocused window
     when isFocused $ applyMouseLook window
 
-    -- Handle movement events
+    -- Handle keyboard movement events
     applyMovement window
 
+    -- Handle Hydra movement events
     case hands of
       [left, right] -> do
         movePlayer (V3 (joystickX left) 0 (-(joystickY left)))
